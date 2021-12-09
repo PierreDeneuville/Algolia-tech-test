d2b4f3dd9864098a428bbc697d08aae3142e1aaa
pierredeneuville <deneuville.pierre@gmail.com>
2021-12-09 17:43:40 +0100

    seed in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "cleaning movie base"
+Movie.destroy_all
+
+puts "importing movies from Algolia JSON"
+
+url = "https://gist.github.com/alexandremeunier/49533eebe2ec93b14d32b2333272f9f8"
+movies_list = URI.open(url).read
+movies = JSON.parse(movies_list)
+
+movies.first(5).each do |movie|
+  puts movie
+end
